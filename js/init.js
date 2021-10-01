290693ef2c01be69c2c47cf64ddbf7633d53f1ab
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-10-02 00:00:47 +0530

    register now ;

--- js/init.js
+++ js/init.js
@@ -49,6 +49,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "HACKATHON ENDED";
+    document.getElementById("timer").innerHTML = "Register Now";
   }
 }, 1000);
